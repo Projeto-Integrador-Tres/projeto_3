eaa4e2cbfa2e5e3f29d56952e117c010456b97fb
jullyanedemetrio <jullyanedmt@gmail.com>
2024-03-19 00:10:57 -0300

    criação do cadastro de usuário

--- website/auth.py
+++ website/auth.py
@@ -1,21 +1,50 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, render_template, flash, redirect, url_for
+from .models import Usuario
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db #DB importado do arquivo init
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+
+    if request.method == 'POST':
+        email = request.form.get('email')
+        senha = request.form.get('senha')
+
+        usuario = Usuario.query.filter_by(email=email).first()
+        if usuario:
+            if check_password_hash(usuario.senha, senha):
+                print('Logado')
+                return redirect(url_for('views.home')) #Futuramente redirecionar para página de conteúdos 
+            else: 
+                print('Senha incorreta')
+        else: 
+            print('E-mail incorreto')
+
     return "Login"
 
 @auth.route('/logout')
 def logout():
     return "logout"
 
-@auth.route('/sign-up', methods=['GET', 'POST'])
+@auth.route('/cadastro-usuario', methods=['GET', 'POST'])
 def sign_up():
 
     if request.method == 'POST':
+        nome = request.form.get('nome')
         email = request.form.get('email')
-        print(email)
+        senha = request.form.get('senha')
+
+        usuario = Usuario.query.filter_by(email=email).first()
+        if usuario:
+            print("Usuário já cadastrado!")
+        else:
+            novo_usuario = Usuario(nome=nome, email=email, senha=generate_password_hash(senha, method='scrypt'))
+            db.session.add(novo_usuario)
+            db.session.commit()
+            #flash('Conta criada com sucesso!') #Futuramente usar flash para criar alertas no html
+            return redirect(url_for('views.home'))
 
 
-    return "Sign Up"
+    return render_template("cadastro_usuario.html")
--- website/models.py
+++ website/models.py
@@ -1,8 +1,11 @@
 from . import db 
 from flask_login import UserMixin
 
-class User(db.Model, UserMixin):
+#User Mixin permite o uso de imports da lib flask_login!
+
+class Usuario(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
+    nome = db.Column(db.String(150))
     email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(150))
-    first_name = db.Column(db.String(150))
+    senha = db.Column(db.String(150))
+    
